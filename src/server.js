52f870b83730866aa0578df27e99043f8ccdce3c
Majd Hasan <majd.hasan@aperto.com>
2020-12-12 18:48:57 +0100

    respone to incoming connections

--- src/server.js
+++ src/server.js
@@ -3,9 +3,16 @@ const socketIO = require('socket.io');
 const server = http.createServer();
 const morgan = require('morgan');
 
+// initialize dotenv
+require('dotenv').config();
+
 // initialize socket io server on port 5001
 const io = socketIO(server);
 const port = process.env.PORT || 5001;
+
+io.on('connection', (socket) => {
+  console.log('New connection has been established with the socket.io server');
+});
 
 server.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
