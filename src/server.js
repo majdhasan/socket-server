edc12fa62e98e4ab522b40aa44252cbe72e29ed9
Majd Hasan <majd.hasan@aperto.com>
2020-12-13 17:54:30 +0100

    add game creation event listener

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const socketIO = require('socket.io');
 const server = http.createServer();
 const morgan = require('morgan');
 const { createId } = require('./utils/helpers');
+const { createGame } = require('./data/games');
+const { createPlayer } = require('./data/players');
 
 // initialize dotenv
 require('dotenv').config();
@@ -15,6 +17,9 @@ io.on('connection', (socket) => {
   console.log('New connection has been established with the socket.io server');
   socket.on('createGame', (data) => {
     console.log(data);
+    const gameId = createId();
+    const player = createPlayer(socket.id, data.name, gameId, 'X');
+    const game = createGame(gameId, player.id, null);
   });
 
   socket.on('joinGame', (data) => {});
