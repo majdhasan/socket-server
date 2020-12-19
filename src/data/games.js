9a574727381286431469e7dd72d95d19f6f22263
Majd Hasan <majd.hasan@aperto.com>
2020-12-19 13:00:16 +0100

    handle box filing, switching turns

--- src/data/games.js
+++ src/data/games.js
@@ -15,4 +15,34 @@ const createGame = (id, player1, player2) => {
   return game;
 };
 
-module.exports = { createGame };
+const getGameIndex = (gameId) => {
+  return games.findIndex((game) => {
+    return game.id == gameId;
+  });
+};
+
+const updateGame = (gameId, boxIndex, playerId) => {
+  const gameIndex = getGameIndex(gameId);
+  const game = games[gameIndex];
+  const symbol = game.player1 === playerId ? 'X' : 'O';
+
+  if (game.player2 && game.player1 && game.playerTurn == playerId) {
+    game.playBoard[boxIndex] = symbol;
+    switchTurn(gameIndex);
+    return game;
+  }
+  return false;
+};
+
+const switchTurn = (gameIndex) => {
+  const game = games[gameIndex];
+  game.playerTurn =
+    game.playerTurn === game.player1 ? game.player2 : game.player1;
+};
+
+const addPlayerToGame = (playerId, gameId) => {
+  const gameIndex = getGameIndex(gameId);
+  games[gameIndex].player2 = playerId;
+};
+
+module.exports = { createGame, updateGame, addPlayerToGame };
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const socketIO = require('socket.io');
 const server = http.createServer();
 const morgan = require('morgan');
 const { createId } = require('./utils/helpers');
-const { createGame } = require('./data/games');
+const { createGame, updateGame } = require('./data/games');
 const { createPlayer } = require('./data/players');
 
 // initialize dotenv
@@ -16,16 +16,26 @@ const port = process.env.PORT || 5001;
 // Handle incoming Websocket connections
 io.on('connection', (socket) => {
   console.log('New connection has been established with the socket.io server');
+
   socket.on('createGame', (data) => {
     console.log(data);
     const gameId = createId();
     const player = createPlayer(socket.id, data.name, gameId, 'X');
     const game = createGame(gameId, player.id, null);
-    socket.emit('playerCreated', { player, game });
+    socket.emit('playerCreated', { player });
+    socket.emit('gameCreated', { game });
   });
 
   socket.on('joinGame', (data) => {
     console.log(data);
+  });
+
+  socket.on('updateGame', (data) => {
+    const { gameId, playerId, box } = data;
+    const result = updateGame(gameId, parseInt(box), playerId);
+    if (result) {
+      socket.emit('gameUpdated', { game: result });
+    }
   });
 
   socket.on('disconnect', () => {
